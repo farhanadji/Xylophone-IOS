324ea9992700a141d81c6920abdcdb9288af2332
Farhan Adji <farhanadji@live.com>
2019-11-26 12:47:40 +0700

    logic completion

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -4,18 +4,38 @@
 //
 //  Created by Angela Yu on 28/06/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
-//
+//  Completed by Farhan Adji on 26/11/2019
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(getKey: sender.currentTitle!)
     }
 
-
+    func playSound(getKey: String){
+        guard let url = Bundle.main.url(forResource: getKey, withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
     
-
 }
 
